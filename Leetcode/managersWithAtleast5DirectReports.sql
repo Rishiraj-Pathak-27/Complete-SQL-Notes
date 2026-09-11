87e1e591af2bfff04eaf400cb15538f3eed8ffb8
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-09-11 22:52:01 +0530

    Refactor code structure for improved readability and maintainability

--- Leetcode/managersWithAtleast5DirectReports.sql
+++ Leetcode/managersWithAtleast5DirectReports.sql
@@ -3,8 +3,8 @@
 -- Return the result table in any order.
 
 SELECT e1.name
-    FROM Employee e1
-    JOIN Employee e2
+FROM Employee e1
+JOIN Employee e2
 ON e1.id = e2.managerId
 GROUP BY e2.managerId
 HAVING COUNT(e2.managerId) >= 5; 
